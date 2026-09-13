ad077c6105e621b602a5f949df729714484c884c
mahmoud <10812023100221@stud.cu.edu.eg>
2026-09-13 23:59:13 +0300

    create InternationalShipment

---
+++ OOP-02/InternationalShipment.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP_02
+{
+    internal class InternationalShipment : Shipment
+    {
+        public string DestinationCountry {
+            get;
+            set {
+                if (DestinationCountry.Length != 0 || DestinationCountry != null)
+                    DestinationCountry = value;
+            } 
+        }
+        public decimal CustomsFee
+        {
+            get;
+            set
+            {
+                if (CustomsFee > 0)
+                    CustomsFee = value;
+            }
+        }
+
+        public override decimal EstimatedCost
+        {
+            get { 
+                return base.EstimatedCost + CustomsFee;
+            }
+        }
+
+
+    }
+}
