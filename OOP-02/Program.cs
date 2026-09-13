607b3ad3c0f2f7514d812306e8c9c5d6db87c619
mahmoud <10812023100221@stud.cu.edu.eg>
2026-09-13 20:40:40 +0300

    Ques 1

--- OOP-02/Program.cs
+++ OOP-02/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Ques 1
+            // a - class can inherit from another class but struct cannot inherit from another struct or class
+            //   - class is reference type and struct is value type
+            //   - class can have a default constructor but struct cannot have a default constructor
+
+            // b - because struct is suitable for small data structures that have value semantics, while class is suitable for larger, more complex objects that require reference semantics.
+
+            #endregion
         }
     }
 }
